a8cf0961ea822dc013c5413812aca92dfff2c509
Bstz-att1 <hfdarioalexander7@gmail.com>
2026-02-09 20:49:31 -0500

    fixed bugs and stuff - estilo mejorado - reacciona ante campo vacio y muestra mensaje

--- app.js
+++ app.js
@@ -3,4 +3,16 @@
     const mensajes = document.getElementById('mensajes');
     const btn = document.getElementById('btn');
 
+    btn.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (text.value.trim() === '') {
+            alert('Debe tener información.');
+            console.log('El campo de mensaje está vacío.');
+            return;
+        }
+        const parrafo = document.createElement("p");
+        parrafo.textContent = text.value;
+        mensajes.appendChild(parrafo);
+    });
+
 
